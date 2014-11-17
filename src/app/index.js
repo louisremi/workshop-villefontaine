e237e1666155764efbac400414a767e69caf22db
louisremi <lrbabe@gmail.com>
2014-11-17 20:18:51 +0100

    First basic version of font-xplor

--- src/app/index.js
+++ src/app/index.js
@@ -2,22 +2,24 @@
 
 angular.module('fontxplor', ['ngAnimate', 'ngResource', 'ngRoute'])
 	.config(function ($routeProvider) {
+		var fonts;
+
 		$routeProvider
-			.when('/', {
+			.when('/:fontName', {
 				templateUrl: 'app/main/main.html',
 				controller: 'MainCtrl',
 				resolve: {
 					allfonts: function( $q ) {
-						return $q(function(resolve) {
+						return fonts || $q(function(resolve) {
 							new FontDetect('font-detect-swf', '/bower_components/font-detect-js/flash/FontList.swf', function(fd) {
-								resolve(fd.fonts());
+								resolve( fonts = fd.fonts() );
 							});
 						});
 					}
 				}
 			})
 			.otherwise({
-				redirectTo: '/'
+				redirectTo: '/_'
 			});
 	})
 ;
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,8 +1,20 @@
 'use strict';
 
 angular.module('fontxplor')
-	.controller('MainCtrl', function ($scope, allfonts) {
+	.controller('MainCtrl', function ($scope, allfonts, $routeParams) {
 
-		console.log(allfonts);
+		$scope.allfonts = {};
+		// convert the font array to a font map
+		allfonts.forEach(function( font ) {
+			$scope.allfonts[font.fontName] = font;
+		});
+
+		// reference to the current font, according to the url
+		$scope.currentFont = $routeParams.fontName !== '_' && $scope.allfonts[ $routeParams.fontName ];
+
+		// build a style object to apply the font-family using ng-style
+		$scope.getFontFamily = function( font ) {
+			return { 'font-family': '"' + font.fontName + '"' };
+		};
 
 	});
